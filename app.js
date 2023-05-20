b2ca04b8662083fc6088c4332835f5446d843da8
aleksandraVkosmose <alexandera.mir0nowa@yandex.ru>
2023-05-21 01:03:15 +0400

    version 1

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const { errors } = require('celebrate');
 const router = require('./routes/router');
+const {
+  createUserValidation,
+  loginValidation,
+} = require('./middlewares/validation');
+const auth = require('./middlewares/auth');
+const {
+  createUser,
+  login,
+} = require('./controllers/users');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -8,14 +18,25 @@ const app = express();
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
 app.use(express.json());
-app.use((request, response, next) => {
-  request.user = {
-    _id: '645ccfe771d2ce3c6993fab1',
-  };
+app.post('/signin', loginValidation, login);
+app.post('/signup', createUserValidation, createUser);
+app.use(auth);
+app.use(router);
+app.use(errors());
+
+app.use((error, request, response, next) => {
+  const {
+    status = 500,
+    message,
+  } = error;
+  response.status(status)
+    .send({
+      message: status === 500
+        ? 'На сервере произошла ошибка'
+        : message,
+    });
   next();
 });
-
-app.use(router);
 
 app.listen(PORT, () => {
   console.log(`Приложение запущено в порте ${PORT}`);
